a3f98df3910c7f80020a1394a6174afd1123ede9
Przemek Saja <przemek.saja@gmail.com>
2018-07-20 13:28:33 +0200

    Make changes

--- components/App.js
+++ components/App.js
@@ -24,25 +24,27 @@ App = React.createClass({
     },
 
     getGif: function(searchingText) {  // 1.
-        return new Promise((resolve, reject) => {
-            var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  // 2.
-            var xhr = new XMLHttpRequest();  // 3. zapytanie do serwera
-            xhr.open('GET', url);
-            xhr.onload = function() {
-                if (xhr.status === 200) {
-                    var data = JSON.parse(xhr.responseText).data; // 4.
-                    var gif = {  // 5.
-                    url: data.fixed_width_downsampled_url,
-                    sourceUrl: data.url
-                    };
-                    resolve(gif);  // 6. przekazujemy obiekt do callback
-                } else {
+        return new Promise(
+            function (resolve, reject) {
+                var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  // 2.
+                var xhr = new XMLHttpRequest();  // 3. zapytanie do serwera
+                xhr.open('GET', url);
+                xhr.onload = function() {
+                    if (xhr.status === 200) {
+                        var data = JSON.parse(xhr.responseText).data; // 4.
+                        var gif = {  // 5.
+                        url: data.fixed_width_downsampled_url,
+                        sourceUrl: data.url
+                        };
+                        resolve(gif);  // 6. przekazujemy obiekt do callback
+                    } else {
                     reject(new Error(xhr.statusText));
-                }
-            };
-            xhr.send();
-        });
-    },
+                    }
+                };
+                xhr.send();
+            });
+        };
+    },    
         
     render: function() {
 
